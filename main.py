40fee572369a04d0cac94bdb02f1b860a8c9e397
darck1971 <darck1971@gmail.com>
2022-04-30 11:18:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# I think I might be going past the level for this stage of the course (module 77 at present) but I wanted to be able to share my progress with my accountability-buddy
+# and have a place to execute all the different things we've done so far.
 from dobcheck import dobcheck
 from driving import driving
 from listslicer import listslicer
@@ -14,7 +16,8 @@ while True:
     0. Exit Program 
 \n""")
   
-  #This is horrible code I know. Python doesn't have case/switch statements (uses match) until 3.10. Replit currently uses 3.08 - I've done it this way and included the way it should be done in comments
+  # This is horrible code I know. Python doesn't have case/switch statements (uses match) until 3.10. Replit currently uses 3.08 - I've done it this way
+  # and included the way it should be done in comments
   if program_choice == "1":
     dobcheck()
   elif program_choice == "2":
@@ -30,7 +33,8 @@ while True:
   else:
       print("Invalid choice")
 
-#Using a multiline string to comment out this code. Python will look at this as a string but since it's not assigned to anything it will ignore it
+# Using a multiline string to comment out this code. Python will look at this as a string but since it's not assigned to anything it will ignore it
+# This way I can just delete the above and remove these quotes and it'll be "right"
 """
 match program_choice:
   case "1":
